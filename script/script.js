2652d58df8a1cff258c0888296ea85c91112dd42
ValerijStojanov <99343041+ValerijStojanov@users.noreply.github.com>
2023-11-06 21:27:39 +0100

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,25 +1,37 @@
 let currentSlide = 1;
-let isPaused = false; // Variable to check if the slideshow is paused
+let isPaused = false; 
 
-// Function to change the slide
 const changeSlide = () => {
   if (!isPaused) {
-    // Remove 'can-zoom' class from all slides
+
     document.querySelectorAll('.zoom-container').forEach((container) => {
       container.classList.remove('can-zoom');
     });
 
-    // Set the active slide
     const activeSlide = document.getElementById(`s${currentSlide}`);
     activeSlide.checked = true;
   }
 };
 
-// Start the auto-scroll
-// let slideInterval = setInterval(changeSlide, 5000);
-
-// Function to pause the auto-scroll
 const pauseSlide = () => {
   isPaused = true;
 };
+
+let burgerMenu = document.querySelector('.burger-menu');
+let navLinks = document.querySelector('.ul_navbar_left_side');
+let bars = document.querySelectorAll('.bar');
+
+let isMenuOpen = false;
+
+burgerMenu.addEventListener('click', () => {
+  isMenuOpen = !isMenuOpen;
+
+  bars.forEach(bar => {
+    bar.style.backgroundColor = isMenuOpen ? 'green' : ''; 
+  });
+});
+
+burgerMenu.addEventListener('click', () => {
+  navLinks.classList.toggle('show-menu');
+});
 
